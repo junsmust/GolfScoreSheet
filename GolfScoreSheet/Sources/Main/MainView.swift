166dc431d241155cee6270a928087247e9860a94
임주영 <isp3087@gmail.com>
2024-05-21 01:16:33 +0900

    참여 인원 바텀시트 생성

--- GolfScoreSheet/Sources/Main/MainView.swift
+++ GolfScoreSheet/Sources/Main/MainView.swift
@@ -8,18 +8,55 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var isPlayPressed: Bool = false
+    
     var body: some View {
-        Button(action: {}, label: {
-            Text("Play")
-                .foregroundStyle(.green)
+        VStack(content: {
+            Button(action: {
+                isPlayPressed.toggle()
+            }, label: {
+                Text("Play")
+                    .foregroundStyle(.green)
+            })
         })
         .padding(10)
         .background(.white)
         .cornerRadius(8.0)
         .shadow(radius: 3)
+        
+        .sheet(isPresented: $isPlayPressed, content: {
+            BottomSheetView()
+                .presentationDetents([.cus])
+                //.presentationDetents([.fraction(0.2)])
+                .presentationDragIndicator(.hidden)
+        })
     }
 }
 
+struct BottomSheetView: View {
+    var body: some View {
+        HStack(content: {
+            Text("플레이는 몇명인가요?")
+                .font(.subheadline)
+                
+            Spacer()
+            HStack(content: {
+                Button(action: {}, label: {
+                    Image(systemName: "minus.circle")
+                })
+                .tint(.black)
+                
+                Text("0")
+                
+                Button(action: {}, label: {
+                    Image(systemName: "plus.circle")
+                })
+                .tint(.black)
+            })
+        })
+        .padding(.horizontal, 20)
+    }
+}
 #Preview {
     MainView()
 }
